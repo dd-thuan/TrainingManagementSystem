6aea0b6affc3cdb0d4448cf14b0ab09b9dddcd82
thuan dinh <thuanddgcd18677@fpt.edu.vn>
2021-06-10 17:06:51 +0700

    ImplementWholeCRUDFunctionsToCourse

--- App/Controllers/StaffController.cs
+++ App/Controllers/StaffController.cs
@@ -1,6 +1,9 @@
 ﻿using App.Models;
+using App.ViewModel;
+using Microsoft.Ajax.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,6 +18,87 @@ namespace App.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult CourseList(string searchString)
+        {
+            var course = _context.courses.Include(t =>t.Category).ToList();
+            if (!searchString.IsNullOrWhiteSpace())
+            {
+                course = _context.courses
+                .Where(c => c.Name.Contains(searchString) || c.Category.Name.Contains(searchString))
+                .Include(c => c.Category)
+                .ToList();
+            }
+            return View(course);
+        }
+
+        [HttpGet]
+        public ActionResult CreateCourse()
+        {
+            var viewModel = new CourseCategoryViewModel()
+            {
+                Categories = _context.categories.ToList()
+            };
+            return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult CreateCourse(CourseCategoryViewModel course)
+        {
+            if (ModelState.IsValid)
+            {
+                var newCourse = new Course()
+                {
+                    Name = course.Courses.Name,
+                    Description = course.Courses.Description,
+                    CategoryId = course.Courses.CategoryId
+                };
+                _context.courses.Add(newCourse);
+                _context.SaveChanges();
+                return RedirectToAction("CourseList");
+            }
+            return View(course);
+        }
+
+        public ActionResult CourseDetails(int id)
+        {
+            var courseInDb = _context.courses.SingleOrDefault(c => c.Id == id);
+            var viewModel = new CourseCategoryViewModel()
+            {
+                Courses = courseInDb,
+                Categories = _context.categories.Where(c => c.Id == courseInDb.CategoryId).ToList()
+            };
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public ActionResult EditCourse(int id)
+        {
+            var courseInDb = _context.courses.SingleOrDefault(c => c.Id == id);
+            var viewModel = new CourseCategoryViewModel()
+            {
+                Courses = courseInDb,
+                Categories = _context.categories.ToList()
+            };
+            return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult EditCourse(CourseCategoryViewModel course)
+        {
+            var courseInDb = _context.courses.SingleOrDefault(c => c.Id == course.Courses.Id);
+            courseInDb.Name = course.Courses.Name;
+            courseInDb.Description = course.Courses.Description;
+            courseInDb.CategoryId = course.Courses.CategoryId;
+            _context.SaveChanges();
+            return RedirectToAction("ListCourse");
+        }
+
+        public ActionResult DeleteCourse(int id)
+        {
+            var courseInDb = _context.courses.SingleOrDefault(c => c.Id == id);
+            _context.courses.Remove(courseInDb);
+            _context.SaveChanges();
+            return RedirectToAction("ListCourse");
         }
     }
 }
